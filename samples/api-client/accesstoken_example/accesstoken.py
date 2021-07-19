975336703115d8a8a0330cde769efd7c9966f093
codepriest <codepriest.gudana@gmail.com>
2021-07-19 04:02:03 +0100

    sort import

--- samples/api-client/accesstoken_example/accesstoken.py
+++ samples/api-client/accesstoken_example/accesstoken.py
@@ -32,15 +32,12 @@ Usage example:
 import argparse
 import io
 import json
-from datetime import datetime, timedelta
 import os
 import time
-
+from datetime import datetime, timedelta
 
 import jwt
-
 import requests as req
-
 
 HOST = "https://cloudiottoken.googleapis.com"
 
@@ -80,7 +77,9 @@ def generate_iot_jwt_token(project_id, algorithm, path_to_private_certificate):
     private_key_bytes = ""
     with io.open(path_to_private_certificate) as f:
         private_key_bytes = f.read()
-    encoded_jwt = jwt.encode(json.loads(jwt_payload), private_key_bytes, algorithm=algorithm)
+    encoded_jwt = jwt.encode(
+        json.loads(jwt_payload), private_key_bytes, algorithm=algorithm
+    )
     return encoded_jwt
     # [END iot_generate_iot_jwt_token]
 
